36e78d9ef400936f64de8848e71813a3115911de
Aitor305 <aitormdm@hotmail.com>
2026-09-12 00:06:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fichero-v1';
+const CACHE = 'fichero-v3';
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
